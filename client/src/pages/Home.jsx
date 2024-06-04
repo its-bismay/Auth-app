31563308f53103c75b9ac451c08499630afef33c
its-bismay <2023ugee070@nitjsr.ac.in>
2024-06-04 20:06:24 +0530

    added content for home and about.Project finished

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,15 @@
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text text-slate-780">
+        Wellcom to my Auth App !!!
+      </h1>
+      <p className="mb-4 text-slate-700">Our new authentication web app is built using the MERN stack (MongoDB, Express, React, and Node.js). It seamlessly integrates Google authentication, allowing users to sign in securely with their Google accounts. Additionally, we have implemented an update feature, enabling users to modify their profile information. The clean and user-friendly design is powered by Tailwind CSS, ensuring a delightful experience for our users. As we continue to enhance the app, we are excited to explore additional features and optimizations.</p>
+      <p className="mb-4 text-slate-700">
+      Our cutting-edge authentication web app incorporates robust security measures. We utilize modern technologies such as JSON Web Tokens (JWTs) for secure user sessions. Cookies play a crucial role in maintaining user state across requests, enhancing the overall experience. To safeguard user credentials, we employ password hashing techniques, ensuring that sensitive information remains confidential. Rest assured, our focus on security sets us apart.
+      </p>
+    </div>
   )
 }
 
